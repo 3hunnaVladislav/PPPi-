9816a8d4b1500777a7acbaea92e2d817f9084e0c
Vladislav Chaika <v.chayka13@gmail.com>
2024-03-03 16:56:39 +0200

    Add subtraction operation

--- Calculator.py
+++ Calculator.py
@@ -2,12 +2,18 @@ class Calculator:
     def add(self, x, y):
         return x + y
 
+    def subtract(self, x, y):
+        return x - y
+
+
 # Тести для класу Calculator
 import pytest
+
 
 @pytest.fixture
 def calculator():
     return Calculator()
+
 
 class TestCalculator:
     def test_add(self, calculator):
@@ -18,3 +24,12 @@ class TestCalculator:
         result = calculator.add(x, y)
         # Assert
         assert result == 8, "Додавання цілих чисел повинно повертати їх суму"
+
+    def test_subtract(self, calculator):
+        # Arrange
+        x = 5
+        y = 3
+        # Act
+        result = calculator.subtract(x, y)
+        # Assert
+        assert result == 2, "Віднімання цілих чисел повинно повертати їх різницю"
